ec975f5be5d4ce6537b0039ea906a5ca52fa31da
Rachit Patel <rachit0706@gmail.com>
2022-10-25 20:34:12 +0530

    Did some changes to js file as it was not working in firefox browser

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-import DATA from "./data.json" assert {type: 'json'};
+// import data from "./data.json" assert {type: 'json'};
+// import assertions not currently supported in the FireFox browser!!
 
-const barChart = document.querySelector('.bar-chart');
+(async () => {
+    const response = await fetch('./data.json');
+    const data = await response.json();
 
-let maxAmount = 0;
-DATA.map(obj => obj.amount).forEach(amount => {
-    if (amount > maxAmount) {
-        maxAmount = amount;
-    }
-});
-
-DATA.forEach(obj => {
-    const barWithDay = document.createElement('div');
-    barWithDay.classList.add('bar-with-day');
-
-    const bar = document.createElement('div');
-    bar.classList.add('bar');
-    bar.style.height = `${obj.amount * 0.12}rem`;
-
-    if (obj.amount === maxAmount) {
-        bar.classList.add("max-bar");
-    }
-    addHoverState(bar, `$${obj.amount}`)
-
-    const day = document.createElement('span');
-    day.classList.add('day');
-    day.textContent = `${obj.day}`;
-
-    barWithDay.append(bar, day);
-    barChart.append(barWithDay);
-});
-
-barChart.style.height = `${barChart.getBoundingClientRect().height + 36}px`;
-
-function addHoverState(node, text) {
-    node.addEventListener('mouseover', function () {
-
-        if (!node.previousSibling) {
-            node.parentNode.insertAdjacentHTML("afterBegin", `<span class="price-bubble">${text}<span>`);
+    const barChart = document.querySelector('.bar-chart');
+    console.log(data);
+    let maxAmount = 0;
+    data.map(async obj => obj.amount).forEach(amount => {
+        if (amount > maxAmount) {
+            maxAmount = amount;
         }
-
     });
-    node.addEventListener('mouseout', function (e) {
 
-        if (node.previousSibling) {
-            node.previousSibling.remove();
+    data.forEach(obj => {
+        const barWithDay = document.createElement('div');
+        barWithDay.classList.add('bar-with-day');
+
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
+        bar.style.height = `${obj.amount * 0.12}rem`;
+
+        if (obj.amount === maxAmount) {
+            bar.classList.add("max-bar");
         }
+        addHoverState(bar, `$${obj.amount}`)
 
+        const day = document.createElement('span');
+        day.classList.add('day');
+        day.textContent = `${obj.day}`;
+
+        barWithDay.append(bar, day);
+        barChart.append(barWithDay);
     });
-}
+
+    barChart.style.height = `${barChart.getBoundingClientRect().height + 36}px`;
+
+    function addHoverState(node, text) {
+        node.addEventListener('mouseover', function () {
+
+            if (!node.previousSibling) {
+                node.parentNode.insertAdjacentHTML("afterBegin", `<span class="price-bubble">${text}<span>`);
+            }
+
+        });
+        node.addEventListener('mouseout', function (e) {
+
+            if (node.previousSibling) {
+                node.previousSibling.remove();
+            }
+
+        });
+    }
+})();
+
+
 
 
 
